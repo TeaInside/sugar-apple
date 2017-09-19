9e663ab50791ef4681849b10776f667237e752cf
Ammar Faizi <ammarfaizi2@gmail.com>
2017-09-19 22:29:17 +0700

    Add more comment

--- src/DB.php
+++ src/DB.php
@@ -1,17 +1,32 @@
 <?php
 
+/**
+ * @author Ammar Faizi <ammarfaizi2@gmail.com>
+ */
+
 class DB
 {
+    /**
+     * @param string $method
+     * @param array  $param
+     * @return PDO
+     */
     public static function __callStatic($method, $param)
     {
         return (new PDO("mysql:host=".DBHOST.";dbname=".DBNAME, DBUSER, DBPASS))->{$method}(...$param);
     }
 
+    /**
+     * @return PDO
+     */
     public static function pdo()
     {
         return (new PDO("mysql:host=".DBHOST.";dbname=".DBNAME, DBUSER, DBPASS));
     }
 
+    /**
+     * @return PDO
+     */
     public static function pdoInstance()
     {
         return (new PDO("mysql:host=".DBHOST.";dbname=".DBNAME, DBUSER, DBPASS));
