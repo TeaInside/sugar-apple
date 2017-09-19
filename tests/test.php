3e714e2e871eb42599f243adb45f8f95147fe49c
Ammar Faizi <ammarfaizi2@gmail.com>
2017-09-19 21:55:59 +0700

    Create simple test

---
+++ tests/test.php
@@ -0,0 +1,20 @@
+<?php
+
+require __DIR__."/../autoload.php";
+
+use Telegram as B;
+
+/**
+ * @author Ammar Faizi <ammarfaizi2@gmail.com>
+ * @see https://core.telegram.org/bots/api
+ */
+
+/**
+ * Contoh ngirim pesan simple.
+ */
+B::sendMessage(
+	[
+		"chat_id" => "@deadinsidegroup",
+		"text"    => "test"
+	]
+);
