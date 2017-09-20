00003f1b402ecd45cc543adb26c3c81924c14306
Ammar Faizi <ammarfaizi2@gmail.com>
2017-09-20 22:37:30 +0700

    Update SaveEvent class

--- autoload.php
+++ autoload.php
@@ -7,4 +7,12 @@ function __load_class($class)
 	require __DIR__."/src/".str_replace("\\", "/", $class).".php";
 }
 
+function pc($exe, $st)
+{
+	if (!$exe) {
+		var_dump($st->errorInfo());
+		die();
+	}
+}
+
 spl_autoload_register("__load_class");
--- src/Handler/SaveEvent.php
+++ src/Handler/SaveEvent.php
@@ -28,6 +28,38 @@ class SaveEvent
 
 	public function save()
 	{
-		
+		if ($this->h->chattype == "private") {
+			$this->private_save();
+		}
+	}
+
+	private function private_save()
+	{
+		$st  = DB::prepare("SELECT `username`,`name`,`msg_count`,`private`,`lang` FROM `a_users` WHERE `userid`=:userid LIMIT 1;");
+		$exe = $st->execute(
+			[
+				":userid" => $this->h->userid
+			]
+		);
+		pc($exe, $st);
+		if ($st = $st->fetch(PDO::FETCH_ASSOC)) {
+			if (
+				$st['username'] != $this->h->username	||
+				$st['name']		!= $this->h->name		||
+				$st['private']	!= "true"
+			) {
+				$st = DB::prepare("UPDATE `a_users` SET `username`=:uname, `name`=:name, `private`='true', `msg_count`=`msg_count`+1 WHERE `userid`=:userid LIMIT 1;");
+				pc($st->execute(
+					[
+						":uname"	=> $this->h->username,
+						":name"		=> $this->h->name,
+						":userid"	=> $this->h->userid
+					]
+				), $st);
+			}
+		}
 	}
 }
+
+
+
