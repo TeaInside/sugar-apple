81289c0729c7d60e11afe473e32c6cf8e9eb98ab
Ammar Faizi <ammarfaizi2@gmail.com>
2017-09-20 22:13:14 +0700

    Debug webhook input

--- connector/cli.php
+++ connector/cli.php
@@ -4,4 +4,6 @@ require __DIR__."/../autoload.php";
 
 $input = file_get_contents("php://input");
 
+file_put_contents("debug.tmp", json_encode(json_decode($input), 128));
+
 shell_exec("nohup /usr/bin/php ".__DIR__."/bg.php \"".urlencode($input)."\" >> ".LOG_DIR."/nohup.out 2>&1");
