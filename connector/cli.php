b7fdfe3a064c6097fe202e817a396bb75510e8b0
Ammar Faizi <ammarfaizi2@gmail.com>
2017-09-20 23:42:57 +0700

    Fix background process

--- connector/cli.php
+++ connector/cli.php
@@ -6,4 +6,4 @@ $input = file_get_contents("php://input");
 
 file_put_contents("debug.tmp", json_encode(json_decode($input), 128));
 
-shell_exec("nohup /usr/bin/php ".__DIR__."/bg.php \"".urlencode($input)."\" >> ".LOG_DIR."/nohup.out 2>&1");
+shell_exec("nohup /usr/bin/php ".__DIR__."/bg.php \"".urlencode($input)."\" >> ".LOG_DIR."/nohup.out 2>&1 &");
