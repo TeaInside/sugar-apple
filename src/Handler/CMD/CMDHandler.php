fae0bca86f4e65712270ff41e061150d73494eee
Ammar Faizi <ammarfaizi2@gmail.com>
2017-09-22 22:39:01 +0700

    Update ban command

--- src/Handler/CMD/CMDHandler.php
+++ src/Handler/CMD/CMDHandler.php
@@ -77,14 +77,14 @@ class CMDHandler
 				$st = DB::prepare(rtrim($query, " OR ").";");
 				pc($st->execute($data), $st);
 				while ($r = $st->fetch(PDO::FETCH_NUM)) {
-					B::kickChatMember(
+					$rrr = json_decode(B::kickChatMember(
 						[
 							"chat_id" => $this->h->chat_id,
 							"user_id" => $r[0]
 						]
-					)['info']['http_code'] == 200 
+					)['info']['content'], true) == ["ok" => true, "result" => true]
 
-					and $msg = "<a href=\"tg://user?id=".$this->h->userid."\">".htmlspecialchars($this->h->first_name)."</a> banned <a href=\"tg://user?id=".$r[0]."\">".htmlspecialchars($r[1])."</a>!" or $msg = "Unknown user.";
+					and $msg = "<a href=\"tg://user?id=".$this->h->userid."\">".htmlspecialchars($this->h->first_name)."</a> banned <a href=\"tg://user?id=".$r[0]."\">".htmlspecialchars($r[1])."</a>!" or $msg = $rrr['description'];
 					B::sendMessage(
 						[
 							"chat_id" => $this->h->chat_id,
