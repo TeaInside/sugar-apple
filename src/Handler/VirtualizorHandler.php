16536cbea37fd3aba57168f7fc976e0e1ca1a5a4
Ammar Faizi <ammarfaizi2@gmail.com>
2017-09-23 20:44:43 +0700

    Make virtualizor handler

---
+++ src/Handler/VirtualizorHandler.php
@@ -0,0 +1,57 @@
+<?php
+
+namespace Handler;
+
+use Telegram as B;
+use Handler\MainHandler;
+use App\Virtualizor\PHP\PHPVirtual;
+use Handler\Security\Virtualizor\PHP;
+
+/**
+ * @author Ammar Faizi <ammarfaizi2@gmail.com>
+ * @license MIT
+ */
+final class VirtualizorHandler
+{
+	/**
+	 * @var Hanlder\MainHanlder
+	 */
+	private $h;
+
+	/**
+	 * Constructor.
+	 * @param Handler\MainHandler $handler
+	 */
+	public function __construct(MainHandler $handler)
+	{
+		$this->h = $handler;
+	}
+
+	public function exec()
+	{
+		$this->__exec();
+	}
+
+	private function __exec()
+	{
+		$t = $this->h->lowertext;
+
+		if (substr($t, 0, 5) == "<?php") {
+			if (PHP::is_secure($this->text)) {
+				$app = new PHPVirtual($this->text);
+				$app = $app->exec();
+				empty($app) and $app = "~";
+				B::sendMessage(
+					[
+						"chat_id" 			  => $this->h->chat_id,
+						"text" 	  			  => $app,
+						"reply_to_message_id" => $this->h->msgid,
+						"parse_mode"		  => "HTML"
+					]
+				);
+			} else {
+				$reject = true;
+			}
+		}
+	}
+}
