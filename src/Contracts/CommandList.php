f25f05f0a4bc5e656539fac625ff716f8837613f
Ammar Faizi <ammarfaizi2@gmail.com>
2017-09-24 08:50:04 +0700

    Create CommandList interface

---
+++ src/Contracts/CommandList.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace Contracts;
+
+use Handler\MainHandler;
+
+interface CommandList
+{
+	public function __construct(MainHandler $handler);
+
+	public function __start($param);
+
+	public function __idan($param);
+
+	public function __idma($param);
+
+	public function __manga($param);
+
+	public function __anime($param);
+
+	public function __ban($param);
+
+	public function __sh($param);
+
+	public function __warn($param);
+
+	public function __welcome($param);
+}
