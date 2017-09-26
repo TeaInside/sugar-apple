46af82f270002393e376512f68f40f4ddc3d049e
Ammar Faizi <ammarfaizi2@gmail.com>
2017-09-26 16:26:35 +0700

    Improve UserChecker.php

--- src/Handler/CMD/UserChecker.php
+++ src/Handler/CMD/UserChecker.php
@@ -2,20 +2,68 @@
 
 namespace Handler\CMD;
 
+use DB;
+use PDO;
 use Handler\MainHandler;
 
 class UserChecker
 {
 	/**
-	 * @var Handler\MainHandler
+	 * @var PDO
 	 */
-	private $h;
+	private $pdo;
 
 	/**
-	 * @param Handler\MainHandler $handler
+	 * @var string
 	 */
-	public function __construct(MainHandler $handler)
+	private $query;
+
+	/**
+	 * @var string
+	 */
+	private $unique;
+
+	/**
+	 * @var string|bool(false)
+	 */
+	private $result;
+
+	/**
+	 * @param string $input
+	 * @param string $type
+	 */
+	public function __construct($input, $type = "username")
 	{
-		$this->h = $handler;
+		$this->input 	= $input;
+		$this->unique	= sha1(time());
+		$this->query 	= "SELECT `{$this->unique}` FROM `a_users` WHERE `{$type}`=:tx LIMIT 1;";
+		$this->pdo		= DB::pdo();
+	}
+
+	private function exec($select)
+	{
+		$st = $this->pdo->prepare(str_replace($this->unique, $select, $this->query));
+		pc($st->execute(
+			[
+				":tx" => $this->input
+			]
+		), $st);
+		if ($st = $st->fetch(PDO::FETCH_NUM)) {
+			return $st[0];
+		} else {
+			return false;
+		}
+	}
+
+	public function getUserID()
+	{
+		$this->exec("userid");
+		return $this->result;
+	}
+
+	public function getUsername()
+	{
+		$this->exec("username");
+		return $this->result;	
 	}
 }
