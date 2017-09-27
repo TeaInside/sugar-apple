ba869a57a019f13e7c57b317a18e98c3c46a0e4e
Ammar Faizi <ammarfaizi2@gmail.com>
2017-09-27 15:57:58 +0700

    Update handler

--- src/Handler/MainHandler.php
+++ src/Handler/MainHandler.php
@@ -6,6 +6,7 @@ use Telegram as B;
 use Handler\Session;
 use Handler\Response;
 use Handler\SaveEvent;
+use Handler\Notification;
 use Handler\CMD\CMDHandler;
 use Handler\VirtualizorHandler;
 
@@ -272,6 +273,8 @@ final class MainHandler
 
     private function save_event()
     {
+        $app = new Notification($this);
+        $app->exec();
         if (in_array($this->msgtype, ["text", "photo", "sticker"])) {
             $se = new SaveEvent($this);
             $se->exec();
